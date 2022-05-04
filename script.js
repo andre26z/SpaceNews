aa38599c949d1fa9e214a02509a06e7ddd25e437
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-05-04 12:20:45 -0300

    up

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const boxpesquisa = document.querySelector(".search");
 const pesquisa = document.querySelector(".input");
 const listanoticias = document.querySelector(".news-list");
+const defaultdata = new Intl.DateTimeFormat("pt-BR");
 
 boxpesquisa.addEventListener("submit", busca);
 
@@ -21,14 +22,23 @@ function busca(e) {
 				console.log(noticias);
 			}
 
-			 jsondata.forEach((article) => {
+			jsondata.forEach((article) => {
 				let li = document.createElement("li");
 				let a = document.createElement("a");
+				let label = document.createElement("label");
+				let img = document.createElement("img");
+				img.src = article.imageUrl;
+				let data = new Date(article.publishedAt);
+				let datacorreta = defaultdata.format(data);
+				console.log(datacorreta);
 				a.setAttribute("href", article.url);
 				a.setAttribute("target", "_blank");
 				a.textContent = article.title;
+				label.textContent = " publicado em: " + "" + datacorreta;
 				li.appendChild(a);
 				listanoticias.appendChild(li);
+				listanoticias.appendChild(img);
+				listanoticias.appendChild(label);
 			});
 		} catch (e) {
 			console.log("error");
