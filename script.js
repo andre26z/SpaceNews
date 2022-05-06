7b77313647da7d964d1b097f7ef4fb27b8aab2af
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-05-06 11:47:23 -0300

    up

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 
 const boxpesquisa = document.querySelector(".search");
 const pesquisa = document.querySelector(".input");
-const listanoticias = document.querySelector(".news-list");
+const listanoticias = document.querySelector(".gallery");
 const defaultdata = new Intl.DateTimeFormat("pt-BR");
 
 boxpesquisa.addEventListener("submit", busca);
 
 function getlimit() {
 	var limite = document.getElementById("select").value;
-	console.log(limite);
+	return limite;
 }
 getlimit();
+console.log(getlimit());
 
 function busca(e) {
 	listanoticias.innerHTML = "";
@@ -20,7 +21,7 @@ function busca(e) {
 	(async function () {
 		try {
 			const url = await fetch(
-				`https://api.spaceflightnewsapi.net/v3/articles?title_contains=${topico}&_limit=100`
+				`https://api.spaceflightnewsapi.net/v3/articles?title_contains=${topico}&_limit=${getlimit()}`
 			);
 			const jsondata = await url.json();
 			console.log(jsondata);
@@ -29,32 +30,33 @@ function busca(e) {
 			}
 
 			jsondata.forEach((article) => {
-				let li = document.createElement("li");
+				let card = document.createElement("div");
 				let a = document.createElement("a");
-				let label = document.createElement("label");
+				let titulo = document.createElement("h2");
 				let img = document.createElement("img");
-				let div = document.createElement("div");
+				let label = document.createElement("label");
 				img.src = article.imageUrl;
+				let bgimage = img.src;
 				let data = new Date(article.publishedAt);
 				let datacorreta = defaultdata.format(data);
-				console.log(datacorreta);
+				// console.log(datacorreta);
+
+				card.setAttribute("class", "gallery-card col-md-2");
+				card.setAttribute("style", "background-image: url(" + bgimage + ")");
 				a.setAttribute("href", article.url);
 				a.setAttribute("target", "_blank");
-				div.setAttribute("class","container");
+				a.setAttribute("class", "gallery-card-btn");
+
 				a.textContent = article.title;
 				label.textContent = " publicado em: " + "" + datacorreta;
-				listanoticias.appendChild(div);
-				div.appendChild(li);
-				li.appendChild(a);
-				li.appendChild(img);
-				li.appendChild(label);
+
+				listanoticias.appendChild(card);
+				card.appendChild(a);
+				card.appendChild(titulo);
+				card.appendChild(label);
 			});
 		} catch (e) {
 			console.log("error");
 		}
 	})();
-
-	// const limit = function maxsearch() {
-
-	// }
 }
